f13a462bc0211d66cc721377ee3ab2dc1e33f50d
mayukhSen12 <mayukhsen448@gmail.com>
2023-02-04 12:44:44 +0530

    E-commerce website

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Layout from './Components/Layout';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<Home/>} />
+          <Route path='about' element={<About/>}/>
+          <Route path='contact' element={<Contact/>}/>
+        </Route>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './App.css';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+      <App />
+);
+reportWebVitals();
